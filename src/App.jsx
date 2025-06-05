22b4a3d7cc474a64444b967faffc0e94aa65d986
KuroHito <dodyroky1212@gmail.com>
2025-06-06 01:18:09 +0700

    perubahan title tools

--- src/App.jsx
+++ src/App.jsx
@@ -38,8 +38,8 @@ function App() {
       </div>
 
       <div className="tools mt-32">
-        <h1 className="text-4xl/snug font-bold mb-4">Tools that i use</h1>
-        <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50">Berikut merupakan tools yang saya sering gunakan</p>
+        <h1 className="text-4xl/snug font-bold mb-4">Tools that I use</h1>
+        <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50"> Here are some of the tools I frequently use in my development workflow.</p>
         <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
 
         {listTools.map((tool) =>(
